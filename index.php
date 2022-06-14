40ce6cc1f9bcce5e908852d0e4fe964c95f52e68
Georges51379 <boutros.georges513@gmail.com>
2022-06-14 16:52:27 +0300

    changing the design

--- index.php
+++ index.php
@@ -1,108 +1,154 @@
 <?php
-session_start();
-// Database Connection File
-require 'db/connection.php';
-if(strlen($_SESSION['email'])==0){
-	header('location:login-user.php');
-}
-else {
-if(isset($_POST['add-project'])){
-		$projectName= $_POST['projectName'];
-		$projectDescription = $_POST['projectDescription'];
-		$dueDate = $_POST['dueDate'];
+require 'dataProcessing.php';
 
-		$query=mysqli_query($con, "insert into project (projectName, projectDescription, dueDate, status)
-		values ('$projectName', '$projectDescription', '$dueDate', 1)");
+if(strlen($_SESSION['email']) == 0){
+	header('location: login-user');
 }
-}
-$userQuery = mysqli_query($con,"SELECT * FROM usertable WHERE email = '".$_SESSION['email']."'");
-$rws = mysqli_fetch_array($userQuery);
+
  ?>
-<html>
-	<head>
-<!--TITLE SECTION-->
-    <title><?php echo htmlentities($rws['name']); ?> | To Do List</title>
-<!--META LINKS-->
-		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
-<!--FONT AWESOME CDN SECTION-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!--jQUERY CDN SECTION-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<!--ICON SECTION-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login & Signup</title>
+		<!-- Bootstrap 4 CSS CDN -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" />
+	 <!-- Fontawesome CSS CDN -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
 		<link href="img/icons/logo.png" rel="shortcut icon">
-<!--INDEX.CSS SECTION-->
-		<link rel="stylesheet" type="text/css" href="css/index.css"/>
-	</head>
+  </head>
 
-	<body>
-			<header>
-				<?php
-				$query = mysqli_query($con, "select * from project where status=1");
-				while($row= mysqli_fetch_array($query)){
-				 ?>
-					 <a class="header-link" href="tasks.php?taskid=<?php echo $row['id']; ?>"><?php echo htmlentities ($row['projectName']); ?>&emsp;
-					 </a>
-			 <?php
-			 }
-				  ?>
-		 </header>
-		 <div class="dark-div">
-			 <button href="#" onclick="darkMode()" class="dark-mode-active"><i class="fa fa-user"></i></button>
-		 </div>
-	 <script>
-		 function darkMode(){
-			 var element = document.body;
-			 element.classList.toggle("dark-mode-active");
-	 }
-	 </script>
+  <body class="bg-info">
+    <div class="container">
+      <!-- Login Form Start -->
+      <div class="row justify-content-center wrapper" id="login-box">
+        <div class="col-lg-10 my-auto myShadow">
+          <div class="row">
+            <div class="col-lg-7 bg-white p-4">
+              <h1 class="text-center font-weight-bold text-primary">Sign in</h1>
+              <hr class="my-3" />
+              <form action="#" method="post" class="px-3" id="login-form">
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="far fa-envelope fa-lg fa-fw"></i></span>
+                  </div>
+                  <input type="email" id="email" name="email" class="form-control rounded-0" placeholder="E-Mail" required />
+                </div>
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="fas fa-key fa-lg fa-fw"></i></span>
+                  </div>
+                  <input type="password" id="password" name="password" class="form-control rounded-0" minlength="5" placeholder="Password" required autocomplete="off" />
+                </div>
+                <div class="form-group clearfix">
+                  <div class="forgot float-right">
+                    <a href="#" id="forgot-link">Forgot Password?</a>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input type="submit" id="login-btn" value="Sign In" class="btn btn-primary btn-lg btn-block myBtn" />
+                </div>
+              </form>
+            </div>
+            <div class="col-lg-5 d-flex flex-column justify-content-center bg-img p-4">
+              <h1 class="text-center font-weight-bold text-white">Welcome Back!</h1>
+              <hr class="my-3 bg-light myHr" />
+              <p class="text-center font-weight-bolder text-light lead">Fill in your infomation to access this application. Have fun!</p>
+              <button class="btn btn-outline-light btn-lg align-self-center font-weight-bolder mt-4 myLinkBtn" id="register-link">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Login Form End -->
+      <!-- Registration Form Start -->
+      <div class="row justify-content-center wrapper" id="register-box" style="display: none;">
+        <div class="col-lg-10 my-auto myShadow">
+          <div class="row">
+            <div class="col-lg-5 d-flex flex-column justify-content-center bg-img p-4">
+              <h1 class="text-center font-weight-bold text-white">Hello User!</h1>
+              <hr class="my-4 bg-light myHr" />
+              <p class="text-center font-weight-bolder text-light lead">To use this application, please fill in the fields.</p>
+              <button class="btn btn-outline-light btn-lg font-weight-bolder mt-4 align-self-center myLinkBtn" id="login-link">Sign In</button>
+            </div>
+            <div class="col-lg-7 bg-white p-4">
+              <h1 class="text-center font-weight-bold text-primary">Create Account</h1>
+              <hr class="my-3" />
+              <form action="#" method="post" class="px-3" id="register-form">
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="far fa-user fa-lg fa-fw"></i></span>
+                  </div>
+                  <input type="text" id="name" name="name" class="form-control rounded-0" placeholder="Full Name" required />
+                </div>
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="far fa-envelope fa-lg fa-fw"></i></span>
+                  </div>
+                  <input type="email" id="remail" name="email" class="form-control rounded-0" placeholder="E-Mail" required />
+                </div>
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="fas fa-key fa-lg fa-fw"></i></span>
+                  </div>
+                  <input type="password" id="rpassword" name="password" class="form-control rounded-0" minlength="5" placeholder="Password" required />
+                </div>
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="fas fa-key fa-lg fa-fw"></i></span>
+                  </div>
+                  <input type="password" id="cpassword" name="cpassword" class="form-control rounded-0" minlength="5" placeholder="Confirm Password" required />
+                </div>
+                <div class="form-group">
+                  <div id="passError" class="text-danger font-weight-bolder"></div>
+                </div>
+                <div class="form-group">
+                  <input type="submit" id="register-btn" value="Sign Up" class="btn btn-primary btn-lg btn-block myBtn" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Registration Form End -->
+      <!-- Forgot Password Form Start -->
+      <div class="row justify-content-center wrapper" id="forgot-box" style="display: none;">
+        <div class="col-lg-10 my-auto myShadow">
+          <div class="row">
+            <div class="col-lg-7 bg-white p-4">
+              <h1 class="text-center font-weight-bold text-primary">Forgot Your Password?</h1>
+              <hr class="my-3" />
+              <p class="lead text-center text-secondary">To reset your password, enter the registered e-mail adddress and we will send you password reset instructions on your e-mail!</p>
+              <form action="#" method="post" class="px-3" id="forgot-form">
+                <div id="forgotAlert"></div>
+                <div class="input-group input-group-lg form-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text rounded-0"><i class="far fa-envelope fa-lg"></i></span>
+                  </div>
+                  <input type="email" id="femail" name="email" class="form-control rounded-0" placeholder="E-Mail" required />
+                </div>
+                <div class="form-group">
+                  <input type="submit" id="forgot-btn" value="Reset Password" class="btn btn-primary btn-lg btn-block myBtn" />
+                </div>
+              </form>
+            </div>
+            <div class="col-lg-5 d-flex flex-column justify-content-center bg-img p-4">
+              <h1 class="text-center font-weight-bold text-white">Reset Password!</h1>
+              <hr class="my-4 bg-light myHr" />
+              <button class="btn btn-outline-light btn-lg font-weight-bolder myLinkBtn align-self-center" id="back-link">Back</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Forgot Password Form End -->
+    </div>
 
-<?php $query= mysqli_query($con,"select name from usertable where email='".$_SESSION['email']."'");
-	$row=mysqli_fetch_array($query);
- ?>
-<center>
-	<div class="wlcm-div">
-		<span class="wlcm-user">welcome,&nbsp;<?php echo $row['name'] ; ?></span><br>
-		<a href="logout-user.php" class="logout">logout<i class="fa fa-sign-out"></i></a>
-	</div>
-	<marquee behavior="scroll" direction="right">
-		this is the main page in which you can add different projects
-	</marquee>
-	<marquee behavior="scroll" direction="left">
-		a reminder will notify you about the due date
-	</marquee>
-	<marquee class="special-marquee" direction="down" scrollamount="2" behavior="scroll" loop="-1">
-		educational and helpful uses only allowed.
-	</marquee>
-<br><br><br>
-<img class="notallowed-gif" src="img/not-allowed.gif">
-<section>
-	<div class="add_project">
-		<form class="form" method="post">
-			<input type="text" class="form_input" name="projectName" placeholder="enter the project name" required/><br><br>
-			<input type="text" class="form_input" name="projectDescription" placeholder="enter the project description" required/><br><br>
-			<input type="date" class="form_input" name="dueDate" placeholder="enter the due date of the project" required/><br><br>
-			<button class="btn_add" name="add-project">Add project</button>
-		</form>
-	</div>
-</section>
-<br><br>
-<?php $query= mysqli_query($con, "select * from banners where form='landscape'");
-$row= mysqli_fetch_array($query); ?>
+    <!-- jQuery CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/script.js"></script>
 
-<div class="banner-landscape">
-	<a href="<?php echo htmlentities($row['url']); ?>" target="_blank">
-	<img class="img-ad-landscape" src="admin/banners/<?php echo htmlentities($row['image']); ?>"
-	data-echo="admin/banners/<?php echo htmlentities($row['image']);?>">
-</a>
-	<br><br>
-	<span class="comp-name">
-		ad by <span class="compName"><?php echo htmlentities($row['companyName']);?></span>
-	</span>
-</div>
-</center>
-
-<?php include 'arrow_to_top.inc.php'; ?>
-<?php include 'footer.php';?>
-
-	</body>
+  </body>
 </html>
