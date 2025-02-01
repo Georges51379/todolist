f631c59debeb903e316fa203f461d0e05e91f7ee
Georges51379 <boutros.georges513@gmail.com>
2025-02-01 12:01:31 +0200

    footer and its links enhanced

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TodoComponent } from './pages/todo/todo.component';
 import { NoteComponent } from './pages/note/note.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { PolicyComponent } from './pages/policy/policy.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'todo', component: TodoComponent },
   { path: 'note', component: NoteComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'policy', component: PolicyComponent },
   { path: '**', redirectTo: 'home' }
 ];
 
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  onSubmit(): void {
+    // Replace this with your form handling logic
+    console.log('Contact Form Submitted:', this.name, this.email, this.message);
+    // Clear form fields or show a success message
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
 }
